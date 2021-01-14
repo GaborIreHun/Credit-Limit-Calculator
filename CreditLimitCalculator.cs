c9def78a70c5c746776aee83202f90413972a743
GaborIreHun <76515173+GaborIreHun@users.noreply.github.com>
2021-01-14 20:00:04 +0000

    Add files via upload
    C# 6 for Programmers, Sixth Edition by Paul Deitel & Harvey Deitel; Excercise 5.18

---
+++ CreditLimitCalculator.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+
+class CreditLimitCalculator
+{
+    public int accountNumber;
+
+    public int initialBalance;
+
+    public int charges;
+
+    public int credit;
+
+    public decimal newBalance;
+
+    
+    public void SetAccountNumber(int creditLimitCalculatorAccountNumber)
+    {
+        accountNumber = creditLimitCalculatorAccountNumber;
+    }
+
+    public int GetAccountNumber()
+    {
+        return accountNumber;
+    }
+
+    public void SetInitialBalance(int creditLimitCalculatorInitialBalance)
+    {
+        initialBalance = creditLimitCalculatorInitialBalance;
+    }
+
+    public int GetInitialBalance()
+    {
+        return initialBalance;
+    }
+
+    public void SetCharges(int creditLimitCalculatorCharges)
+    {
+        charges = creditLimitCalculatorCharges;
+    }
+
+    public int GetCharges()
+    {
+        return charges;
+    }
+
+    public void SetCredit(int creditLimitCalculatorCredit)
+    {
+        credit = creditLimitCalculatorCredit;
+    }
+
+    public int GetCredit()
+    {
+        return credit;
+    }
+
+    public decimal NewBalance()
+    {
+        newBalance = initialBalance + credit - charges;
+
+        if (newBalance < 0)
+        {
+            Console.WriteLine("Credit limit exceeded");
+        }
+
+        return newBalance;
+    }
+    
+}
---
+++ CreditLimitCalculatorTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class CreditLimitCalculatorTest
+{
+    
+
+    static void Main()
+    {
+        CreditLimitCalculator creditLimitCalculator = new CreditLimitCalculator();
+
+
+        
+
+        while (true)
+        {
+            Console.Write("Please enter the relevant account number or -1 to quit: ");
+            int creditLimitCalculatortheAccountNumber = int.Parse(Console.ReadLine());
+            creditLimitCalculator.SetAccountNumber(creditLimitCalculatortheAccountNumber);
+
+            if (creditLimitCalculatortheAccountNumber == -1)
+                break;
+
+            Console.Write("Please enter the relevant beginning balance: ");
+            int creditLimitCalculatortheInitialBalance = int.Parse(Console.ReadLine());
+            creditLimitCalculator.SetInitialBalance(creditLimitCalculatortheInitialBalance);
+
+            Console.Write("Please enter the relevant charges: ");
+            int creditLimitCalculatortheCharges = int.Parse(Console.ReadLine());
+            creditLimitCalculator.SetCharges(creditLimitCalculatortheCharges);
+
+            Console.Write("Please enter the relevant credits: ");
+            int creditLimitCalculatortheCredit = int.Parse(Console.ReadLine());
+            creditLimitCalculator.SetCredit(creditLimitCalculatortheCredit);
+
+            Console.WriteLine($"The customer's new balance is: {creditLimitCalculator.NewBalance()}");
+
+            
+        }
+
+        Console.WriteLine("Thank you and see you later!");
+
+        
+    }
+
+
+}
+
